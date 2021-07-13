f6e2401adfc3cb5c20dadc717efd9f55aa8646ae
John Bonham <sabashlive@gmail.com>
2021-07-13 10:27:04 +0300

    fix cmd for nvim (path config)

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -4,5 +4,5 @@ mkdir ~/.config/nvim
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/init.vim ~/config/nvim/init.vim
+ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 echo "[*] Done inject files"
