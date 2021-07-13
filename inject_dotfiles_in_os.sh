b49cd6e2aa8064a4bf04f68fb392586df7c209aa
John Bonham <sabashlive@gmail.com>
2021-07-13 10:18:12 +0300

    add cmd for nvim

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "[*] Start inject files in system"
+mkdir ~/.config/nvim
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/init.vim ~/config/nvim/init.vim
 echo "[*] Done inject files"
