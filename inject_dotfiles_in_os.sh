bd1f09bc5a486419d32d5c30608622fcfbdd2e9b
John Bonham <sabashlive@gmail.com>
2021-11-30 15:05:35 +0300

    add tmux conf file

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 echo "[*] Start inject files in system"
 mkdir ~/.config/nvim
@@ -5,4 +6,5 @@ ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 echo "[*] Done inject files"
