5ccb939df97d85b4c74de6555ab6939c90aac77c
John Bonham <sabashlive@gmail.com>
2022-01-18 11:30:04 +0300

    add command for inject Powerlevel10k file

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 echo "[*] Start inject files in system"
+
 mkdir ~/.config/nvim
 mkdir ~/.config/alacritty
 mkdir ~/.config/vifm
+
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/p10k.zsh ~/.p10k.zsh
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/vifmrc ~/.config/vifm/vifmrc
+
 echo "[*] Done inject files"
