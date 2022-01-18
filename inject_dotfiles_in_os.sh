0d0cf9a06db7fb6361b6ed12ccb84ab7d0833916
John Bonham <sabashlive@gmail.com>
2022-01-18 08:59:26 +0300

    create dir for conf files

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "[*] Start inject files in system"
 mkdir ~/.config/nvim
+mkdir ~/.config/alacritty
+mkdir ~/.config/vifm
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
