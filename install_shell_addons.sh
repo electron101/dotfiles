7641f1d2d7ff502844d98a1a425b9c6fd0e799d6
John Bonham <sabashlive@gmail.com>
2022-01-18 11:52:09 +0300

    download vifm-colors

--- install_shell_addons.sh
+++ install_shell_addons.sh
@@ -19,4 +19,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 # В связке с Oh My Zsh:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
+# === [ Vifm-colors ] ===
+git clone https://github.com/vifm/vifm-colors ~/.config/vifm/colors
+
 echo "[*] Done install shell addons"
