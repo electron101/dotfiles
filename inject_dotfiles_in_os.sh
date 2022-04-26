7c4150d94894b162239b2d8310569a489492129c
John Bonham <sabashlive@gmail.com>
2022-04-26 16:27:14 +0300

    Правка для инъекции новых конфигов nvim. Исправлена ошибка mkdir (папка уже существует).

--- inject_dotfiles_in_os.sh
+++ inject_dotfiles_in_os.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "[*] Start inject files in system"
+echo "[*] Start inject dot files in system"
 
-mkdir ~/.config/nvim
-mkdir ~/.config/alacritty
-mkdir ~/.config/vifm
+#mkdir -p ~/.config/nvim
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/vifm
 
 ln -sf ~/.dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
 ln -sf ~/.dotfiles/p10k.zsh ~/.p10k.zsh
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/nvim ~/.config
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/vifmrc ~/.config/vifm/vifmrc
 
