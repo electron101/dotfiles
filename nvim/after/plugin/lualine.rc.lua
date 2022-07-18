193428e80551edf0bfc90c087ee81cc4cd31b8de
John Bonham <sabashlive@gmail.com>
2022-07-18 11:22:52 +0300

    Переписано с помощью virtcol(), так как nvim_win_get_cursor считает символ Tab за один символ и unicode за два символа.

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -1,11 +1,14 @@
 local status, lualine = pcall(require, "lualine")
 if (not status) then return end
 
--- Отображет позицию курсора и общее количество строк ( 70/2034:0 )
+-- Отображет позицию курсора и общее количество строк ( 70/2034:0 ). Переписано
+-- с помощью virtcol(), так как nvim_win_get_cursor считает символ Tab за один
+-- символ и unicode за два символа.
 local function my_location()
+	local cur_col    = vim.fn.virtcol('.')
 	local r,c        = unpack(vim.api.nvim_win_get_cursor(0))
 	local total_rows = vim.api.nvim_buf_line_count(0)
-	return string.format("%+3s/%s:%-2s", r, total_rows, c);
+	return string.format("%+3s/%s:%-2s", r, total_rows, cur_col);
 end
 
 lualine.setup {
@@ -35,7 +38,7 @@ lualine.setup {
     },
     lualine_y = {'progress'},
     lualine_z = {
-			-- 'location', 
+			-- 'location', }
 			my_location }
   },
   inactive_sections = {
