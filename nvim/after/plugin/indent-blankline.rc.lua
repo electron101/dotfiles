39e171f2c11193b3b573c1639b832e63dbd87175
John Bonham <sabashlive@gmail.com>
2023-09-28 14:43:26 +0300

    fix: Good char '|' for vertical line

--- nvim/after/plugin/indent-blankline.rc.lua
+++ nvim/after/plugin/indent-blankline.rc.lua
@@ -1,3 +1,7 @@
 require("ibl").setup({
     scope = { enabled = false },
+    indent = {
+        char = "│", -- https://github.com/lukas-reineke/indent-blankline.nvim/issues/282
+        smart_indent_cap = true,
+    },
 })
